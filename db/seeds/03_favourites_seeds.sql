0d2d3950d06d3835c416b4f07c53b030f7580ae7
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-01-10 19:03:52 +0000

    add tester to users, create query for favourite items by user

--- db/seeds/03_favourites_seeds.sql
+++ db/seeds/03_favourites_seeds.sql
@@ -2,4 +2,3 @@
 INSERT INTO favourites(user_id, item_id) VALUES (1, 3);
 INSERT INTO favourites(user_id, item_id) VALUES (1, 5);
 
-
---
+++ queries/3_favourited_items.sql
@@ -0,0 +1,9 @@
+--- Get a list of all favourited items for specific user
+
+SELECT items.*
+FROM items
+JOIN favourites ON item_id = items.id
+JOIN users ON owner_id = users.id
+--- WHERE users.id = 2 shows list of favourites for user 2, but user 2 does not have favourites, users.id = 1 does not populate
+WHERE user_id = 4-- this is the buyer id
+ORDER BY items.date_posted, items.title;
