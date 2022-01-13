9c8818631759acfd7b233b1b21b78f3e530b18fa
Andrew Bidylo <bidyloandrew@gmail.com>
2022-01-13 21:44:13 +0000

    Add mark sold, add new items

--- db/schema/02_items.sql
+++ db/schema/02_items.sql
@@ -7,7 +7,7 @@ CREATE TABLE items (
   title VARCHAR(50) NOT NULL,
   location VARCHAR(50),
   price SMALLINT NOT NULL,
-  description VARCHAR(250) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
   thumbnail_photo_url VARCHAR(1000) NOT NULL,
   date_posted DATE,
   date_sold DATE DEFAULT NULL
--- routes/items.js
+++ routes/items.js
@@ -11,14 +11,6 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-// //--Login--//
-
-//   router.get('/login/:id', (req, res) => {
-//   const userType = req.params.id === 1 ? 'buyer' : 'seller';
-//   req.session.user_id = req.params.id;
-//   req.session.user_type = userType;
-//   res.redirect('/');
-// });
 
 router.post("/new", (req, res) => {
   // const params = req.query.params;
@@ -72,7 +64,7 @@ router.get("/new",(req,res) => {
       queryString += ` AND price <= $${queryParams.length}`;
     }
 
-  queryString += `ORDER BY price, title;`;
+  queryString += `ORDER BY date_posted DESC, title;`;
 
     db.query(queryString, queryParams)
       .then(data => {
@@ -103,7 +95,9 @@ router.get("/new",(req,res) => {
     db.query(query)
       .then(data => {
         const items = data.rows[0];
+        console.log(items)
         const email = data.rows[0].email;
+        
         res.render('item_details', {items, itemId, id, email});
       })
       .catch(err => {
@@ -115,20 +109,18 @@ router.get("/new",(req,res) => {
 
 
 
-  router.post("/sold", (req, res) => {
+  router.post("/:id/sold", (req, res) => {
     // const params = req.query.params;
     const itemId = req.params.id;
     const owner_id = parseInt(req.session.user_id);
-console.log(itemId, owner_id)
     let query = `
      UPDATE items
-     SET date_sold = '2000-10-20'
+     SET date_sold = CURRENT_DATE
      WHERE items.id = ${itemId} AND owner_id = ${owner_id};`;
 
     db.query(query)
       .then(data => {
-        // res.redirect("/items");
-        // console.log('ADD NEW FAVE - POST');
+        res.redirect("/items");
       })
       .catch(err => {
         res
@@ -137,149 +129,9 @@ console.log(itemId, owner_id)
       });
   });
 
-  //--GET ONE ITEM--//
-
-//   router.get("/:id", (req, res) => {
-//     const userId = req.session.user_id;
-//     const id = req.params.id;
-// console.log(req.params)
-//     let query = `
-//     SELECT *
-//     FROM items
-//     WHERE id = ${id};`;
-
-//     db.query(query)
-//       .then(data => {
-
-//         const items = data.rows;
-
-//         res.send({items, userId});
-
-//       })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .json({ error: err.message });
-//       });
-//   });
-
-  //--GET ITEM BY PRICE--//
-
-  // router.get("/", (req, res) => {
-  //   let query = `
-  //   SELECT *
-  //   FROM items
-  //   WHERE price BETWEEN 10 AND 100
-  //   ORDER BY price, title;`;
-  //   db.query(query)
-  //     .then(data => {
-  //       const items = data.rows;
-  //       res.send({ items });
-  //       console.log('GET ITEM BY PRICE');
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-
-  //--GET ALL FAVES--// in user.js file move
-
-  // router.get("/favourites", (req, res) => {
-  //   const id = req.session.user_id;
-  //   const user = users[id];
-  //   console.log('ID:', id);
-  //   console.log('U:', user);
-  //   let query = `
-  //   SELECT items.*
-  //   FROM items
-  //   JOIN favourites ON item_id = items.id
-  //   JOIN users ON owner_id = users.id
-  //   WHERE user_id = 1
-  //   ORDER BY items.date_posted, items.title;`;
-
-  //   db.query(query)
-  //     .then(data => {
-  //       const items = data.rows;
-  //       res.render('favourites', { items });
-  //       console.log('FAVE ITEMS LIST');
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-  //--ADD TO FAVES--// in favourites.js, pass favourite obj data into body:
 
 
 
-
-//--DELETE FROM FAVES--//
-
-  router.delete("/favourites/:id", (req, res) => {
-    let query = `
-    DELETE FROM favourites
-    WHERE item_id = 5 AND user_id = 1;`;
-
-    db.query(query)
-      .then(data => {
-        const items = data.rows;
-        res.send({ items });
-        console.log('REMOVE FAVE');
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  //--GET ADD NEW ITEM FORM--//
-
-
-  //user id passed through body
-  //res.render form
-
-  //--ADD NEW ITEM--//
-
-  // router.post("/new", (req, res) => {
-  //   let query = `
-  //   INSERT INTO items (owner_id, title, location, price, description, thumbnail_photo_url, date_posted) VALUES (2, 'CELL2', 'Toronto', 20000, 'message', 'https://i.imgur.com/96St5p8.jpeg', '2022-01-10');`;
-
-  //   db.query(query)
-  //     .then(data => {
-  //       const items = data.rows;
-  //       res.send({ items });
-  //       console.log('ADD NEW FAVE - POST');
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-
-
-
-//   router.post('/properties', (req, res) => {
-//     const userId = req.session.userId;
-//     database.addProperty({...req.body, owner_id: userId})
-//       .then(property => {
-//         res.send(property);
-//       })
-//       .catch(e => {
-//         console.error(e);
-//         res.send(e)
-//       });
-//   });
-
-//   return router;
-// }
   //--DELETE ITEM FROM LISTINGS--//
 
   //router.delete
